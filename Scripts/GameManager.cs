a402f58590cf08dedabda91efb657444bda28e4e
stgd21 <45217325+stgd21@users.noreply.github.com>
2020-09-09 19:54:34 -0700

    Add files via upload

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Linq;
+
+public class GameManager : MonoBehaviourPun
+{
+    [Header("Players")]
+    public string playerPrefabLocation;
+    public PlayerController[] players;
+    public Transform[] spawnPoints;
+    public int alivePlayers;
+
+    private int playersInGame;
+
+    //instance
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        players = new PlayerController[PhotonNetwork.PlayerList.Length];
+        alivePlayers = players.Length;
+
+        photonView.RPC("ImInGame", RpcTarget.AllBuffered);
+    }
+
+    //called to all players when someone loads into the game scene.
+    //keeps track of how many players are in the game and if everyone is in, spawn in the players
+    [PunRPC]
+    void ImInGame()
+    {
+        playersInGame++;
+        if (PhotonNetwork.IsMasterClient && playersInGame == PhotonNetwork.PlayerList.Length)
+            photonView.RPC("SpawnPlayer", RpcTarget.All);
+    }
+
+    //Instantiates a player across the network
+    [PunRPC]
+    void SpawnPlayer()
+    {
+        GameObject playerObj = PhotonNetwork.Instantiate(playerPrefabLocation, spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity);
+
+        //initialize player for all other players
+        playerObj.GetComponent<PlayerController>().photonView.RPC("Initialize", RpcTarget.All, PhotonNetwork.LocalPlayer);
+    }
+}
---
+++ Scripts/NetworkManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public int maxPlayers = 10;
+
+    public static NetworkManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        //connect to master server
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to master server");
+    }
+
+    //attempt to create room
+    public void CreateRoom(string roomName)
+    {
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = (byte)maxPlayers;
+
+        PhotonNetwork.CreateRoom(roomName, options);
+    }
+
+    //attempt to join room
+    public void JoinRoom(string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    //change scene
+    [PunRPC]
+    public void ChangeScene(string sceneName)
+    {
+        PhotonNetwork.LoadLevel(sceneName);
+    }
+}
